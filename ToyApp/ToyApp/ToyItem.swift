c46412a43b90b46d8fc8fb436a2a18722a80687d
Erica-de-O-Veiga <ericadeoliveiraveiga@gmail.com>
2023-02-26 16:16:39 -0300

    Versao1

---
+++ ToyApp/ToyApp/ToyItem.swift
@@ -0,0 +1,19 @@
+//
+//  ToyItem.swift
+//  ToyApp
+//
+//  Created by Erica Veiga on 24/02/23.
+//
+
+import Foundation
+
+struct ToyItem{
+    var id : String
+    var nome: String
+    var estado: String
+    var doador: String
+    var endereco: String
+    var telefone: String
+    
+}
+

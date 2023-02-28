8bfeef839a2802f68d886b659e1d88ae4b232c60
Erica-de-O-Veiga <ericadeoliveiraveiga@gmail.com>
2023-02-28 10:18:30 -0300

    Projeto com Firebase - erro

---
+++ Projeto1/Projeto1/Projeto1/BrinquedoItem.swift
@@ -0,0 +1,18 @@
+//
+//  BrinquedoItem.swift
+//  Projeto1
+//
+//  Created by Pablo Veiga on 28/02/23.
+//
+
+import Foundation
+
+struct BrinquedoItem{
+    var id : String
+    var nome: String
+    var estado: String
+    var doador: String
+    var endereco: String
+    var telefone: String
+    
+}
